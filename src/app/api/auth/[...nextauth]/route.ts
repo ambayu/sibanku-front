a1345ec2d4adc5c2f8a3ec51f78a5ead052a916f
Bayu <am.bayu07@gmail.com>
2025-10-13 14:21:43 +0700

    asd

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,7 @@ const handler = NextAuth({
             },
             async authorize(credentials) {
                 try {
-                    console.log("🔍 Fetching:", `${process.env.NEXT_PUBLIC_API_URL}/auth/login`);
+                    console.log("🔍 Fetching:", `http://127.0.0.1:4000/auth/login`);
                     // 1. Login → ambil token
                     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
                         method: "POST",
