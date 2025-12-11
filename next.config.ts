da60cdbccd4ee456b3cc570fce0b76364c338691
ambayu <am.bayu07@gmail.com>
2025-12-11 17:07:36 +0700

    asd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Abaikan semua error lint saat build
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
